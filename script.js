1b719ffecef4397140d943e0045a0325e425ba55
susthir <susthir19march@gmail.com>
2024-03-05 23:58:44 +0530

    js code

---
+++ script.js
@@ -0,0 +1,18 @@
+
+//simple structure of a function
+function myFun(){
+    console.log("hy this is susthir");
+}
+myFun();
+
+//arrow function
+const mulFun = (a,b) =>{
+    return(a*b);
+} 
+function voulcount( str){
+    let str="awerteioyutrtrgdeey"
+    for(const char of str){
+        console.log(char);  
+    }
+
+}
