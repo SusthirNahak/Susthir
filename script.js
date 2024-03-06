51bbe7f6f7e5cb95b07ed54774ec4ddc321ba48e
susthir <susthir19march@gmail.com>
2024-03-06 13:03:21 +0530

    lets go

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
-
-//simple structure of a function
-function myFun(){
-    console.log("hy this is susthir");
+const menubar = document.querySelector('#menu');
+const Navbar = document.querySelector('.navbar');
+menubar.onclick=()=>{
+    menubar.classList.toggle('bx-x');
+    Navbar.classList.toggle('active')
 }
-myFun();
-
-//arrow function
-const mulFun = (a,b) =>{
-    return(a*b);
-} 
-function voulcount( str){
-    let str="awerteioyutrtrgdeey"
-    for(const char of str){
-        console.log(char);  
-    }
-
+const section=document.querySelectorAll('section');
+const navlink = document.querySelectorAll('header nav a')
+window.onscroll = ()=>{
+    section.forEach(sec=>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id')
+        if(top>offset && top < offset + height){
+            sec.classList.add('start-animation');
+            navlink.forEach(links=>{
+                links.classList.remove('active')
+                document.querySelector('header nav a[href*='+id+']').classList.add('active')
+              
+            })
+        }
+    })
+    var header = document.querySelector('.header');
+    header.classList.toggle('sticky',window.scrollY>100)
+    menubar.classList.remove('bx-x');
+    Navbar.classList.remove('active')
 }
